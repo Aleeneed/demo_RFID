006c1e652c06d19143b4f5414f222a1b3e2f859c
Aleeneed <94275973+Aleeneed@users.noreply.github.com>
2022-11-30 00:29:07 +0800

    Update RFID_Sql_Append.py

--- RFID_Project/RFID_Sql_Append.py
+++ RFID_Project/RFID_Sql_Append.py
@@ -6,7 +6,7 @@ from threading import Timer
 connection=mysql.connector.connect(host='127.0.0.1',
                                         port='3306',
                                         user='root',
-                                        password='jerry860327',
+                                        password='root',
                                         database='Rfid_databases',
                                         buffered=True)
 cursor= connection.cursor()
