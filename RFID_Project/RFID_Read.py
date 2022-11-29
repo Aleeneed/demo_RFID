190eedc55d8072930c8b8092b948f4d455a05cb9
Aleeneed <94275973+Aleeneed@users.noreply.github.com>
2022-11-30 00:26:33 +0800

    Update RFID_Read.py

--- RFID_Project/RFID_Read.py
+++ RFID_Project/RFID_Read.py
@@ -1,13 +1,13 @@
 import serial
+import time
 
-def rfid(): #read RFID Data
-    ser = serial.Serial()
-    ser.baudrate = 9600    
-    ser.port = 'com10' #connect port
-    ser.open() #open serial
+ser = serial.Serial()
+ser.port = 'COM10' 
+ser.open()     
+def rfid():
     RFID_Data=ser.readline() 
     RFID_Data = RFID_Data.decode()  
-    RFID_Data = RFID_Data.strip() 
+    RFID_Data = RFID_Data.strip()
     return(RFID_Data)
 
 if __name__ =="__main__":
